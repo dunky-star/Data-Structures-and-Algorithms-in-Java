6ab17c18aa1b5b40e27cea0c8652120627ffb15f
dunky-star <dukygeoffrey39@gmail.com>
2022-11-25 16:33:15 +0300

    Big O(n+n)

--- DSA-JAVA/src/datastructure/misc/big_o/Main.java
+++ DSA-JAVA/src/datastructure/misc/big_o/Main.java
@@ -2,10 +2,13 @@ package datastructure.misc.big_o;
 
 public class Main {
     // O(n)
+    // O(n+n)
     public static void printItems(int n){
         for(int i=0; i < n; i++){
             System.out.println(i);
-
+        }
+        for(int j=0; j < n; j++){
+            System.out.println(j);
         }
     }
 
