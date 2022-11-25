2a2eb5d90e131a21e396ae60efbd3ec477d55e87
dunky-star <dukygeoffrey39@gmail.com>
2022-11-25 19:43:21 +0300

    Big O: different terms for inputs

--- DSA-JAVA/src/datastructure/misc/big_o/Main.java
+++ DSA-JAVA/src/datastructure/misc/big_o/Main.java
@@ -2,8 +2,14 @@ package datastructure.misc.big_o;
 
 public class Main {
     // Big O(n)
+    public static void printItem_n(int n){
+        for(int i=0; i < n; i++){
+            System.out.println(i);
+        }
+
+    }
     // Big O(n+n)
-    public static void printItemsn(int n){
+    public static void printItems_n(int n){
         for(int i=0; i < n; i++){
             System.out.println(i);
         }
@@ -13,7 +19,7 @@ public class Main {
     }
 
     // Big O(n^3)
-    public static void printItemsn_n(int n){
+    public static void printItems_n_n(int n){
         for(int i=0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k=0; k < n; k++) {
@@ -29,13 +35,26 @@ public class Main {
         return n + n + n;
     }
 
-    public static void main(String[] args){
-        /*
-        * System.out.println("The Big O(n): " + printItemsn(10));
-        *
-        *System.out.println("The Big O(n^2): " + printItemsn_n(10));
-        */
+    // Big O(a + b)
+    public static void printItem_a_b(int a, int b){
+        for(int i=0; i < a; i++){
+            System.out.println(i);
+        }
+        for(int j=0; j < b; j++){
+            System.out.println(j);
+        }
+    }
 
-        System.out.println("The Big O(1): " + addItems(10));
+    public static void main(String[] args){
+
+        // System.out.println("The Big O(n): ");
+        // printItem_n(10);
+
+        // System.out.println("The Big O(n^2): " + printItems_n_n(10));
+
+
+        // System.out.println("The Big O(1): " + addItems(10));
+        System.out.println("The Big O(a + b): ");
+        printItem_a_b(5, 8);
     }
 }
