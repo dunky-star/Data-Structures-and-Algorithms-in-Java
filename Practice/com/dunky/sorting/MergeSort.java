a4bfc1a4a716f51abf2d4fa5c2734f3f51661f73
dunky-star <dukygeoffrey39@gmail.com>
2023-01-20 19:08:55 +0300

    Merge sort

--- Practice/com/dunky/sorting/MergeSort.java
+++ Practice/com/dunky/sorting/MergeSort.java
@@ -1,20 +1,33 @@
 package com.dunky.sorting;
 
+/**
+ * MERGE_SORT(arr, beg, end)
+ *
+ * if beg < end
+ * set mid = (beg + end)/2
+ * MERGE_SORT(arr, beg, mid)
+ * MERGE_SORT(arr, mid + 1, end)
+ * MERGE (arr, beg, mid, end)
+ * end of if
+ *
+ * END MERGE_SORT
+ */
+
 public class MergeSort {
 
-    public void mergeSort(int[] array, int start, int end){
-        if(start < end){
-            int middle = (end - start)/2 + start;
-            mergeSort(array, start, end);
-            mergeSort(array, middle + 1, end);
-            merge(array, start, middle, end);
+    // Merge sort method
+    private void mergeSort(int a[], int beg, int end)
+    {
+        if (beg < end)
+        {
+            int mid = (beg + end) / 2;
+            mergeSort(a, beg, mid);
+            mergeSort(a, mid + 1, end);
+            merge(a, beg, mid, end);
         }
     }
 
-    public void mergeSort(int[] array){
-        mergeSort(array, 0, array.length - 1);
-    }
-
+    // Merge method
     private void merge(int[] array, int start, int middle, int end){
         int i = start;
         int j = middle + 1;
@@ -29,5 +42,31 @@ public class MergeSort {
             }
         }
         System.arraycopy(arrayTemp, 0, array, start, arrayTemp.length);
+    }
+
+    /* Function to print the array */
+    void printArray(int a[])
+    {
+         for (int i = 0; i < a.length; i++) {
+             System.out.print(a[i] + " ");
+         }
+    }
+
+    // Main function
+    public static void main(String[] args){
+
+        // Array to sort
+        int a[] = { 11, 30, 24, 7, 31, 16, 39, 41 };
+
+        MergeSort mClass = new MergeSort();
+        System.out.println("\nBefore sorting array elements are - ");
+        mClass.printArray(a);
+
+        // Applying the Merge Sort
+        mClass.mergeSort(a, 0, a.length - 1);
+        System.out.println("\nAfter sorting array elements are - ");
+        mClass.printArray(a);
+        System.out.println("");
+
     }
 }
