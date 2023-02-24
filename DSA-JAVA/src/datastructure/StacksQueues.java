b2e8fa8993ef83b7ecf8ba83efe34401fd58df8c
dunky-star <dukygeoffrey39@gmail.com>
2023-02-24 13:55:37 +0300

    Stacks and Queues

---
+++ DSA-JAVA/src/datastructure/StacksQueues.java
@@ -0,0 +1,28 @@
+package datastructure;
+
+// We use ArrayList or Linked List for implementing Stack data structure.
+
+public class StacksQueues {
+    // private member variables.
+    private Node top;
+    private int height;
+
+    // Inner class for node
+    static class Node{
+        int value;
+        Node next;
+
+        Node (int value){
+            this.value = value;
+        }
+    }
+
+    // Stack, Queue Constructor
+    public StacksQueues(int value){
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+
+}
