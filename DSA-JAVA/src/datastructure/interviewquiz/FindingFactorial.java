77cdfe7133994936e728cd04dd2915c35aa2d088
dunky-star <dukygeoffrey39@gmail.com>
2023-02-25 12:01:53 +0300

    Finding factorial of a number

---
+++ DSA-JAVA/src/datastructure/interviewquiz/FindingFactorial.java
@@ -0,0 +1,16 @@
+package datastructure.interviewquiz;
+
+/**
+ * Finding factorial of a number.
+ */
+
+public class FindingFactorial {
+    public static int factorial(int n){
+        if(n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args){
+       System.out.println("The factorial is: " + factorial(10)) ;
+    }
+}
