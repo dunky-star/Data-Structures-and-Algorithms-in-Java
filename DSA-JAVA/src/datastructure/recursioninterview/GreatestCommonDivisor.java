adc1c08da8634cfd6f770a5b78c61c3da7b8478c
dunky-star <dukygeoffrey39@gmail.com>
2023-02-27 14:30:59 +0300

    Cracking recursion interview quiz - Greatest Common Divisor

---
+++ DSA-JAVA/src/datastructure/recursioninterview/GreatestCommonDivisor.java
@@ -0,0 +1,21 @@
+package datastructure.recursioninterview;
+
+/**
+ * How to find GCD of two numbers using recursion?
+ * Eg. GCD(8,12) = 4
+ * Best way is using Euclidean Algorithm.
+ */
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args){
+      System.out.println("GCD: " + gcd(8, 4));
+    }
+
+    public static int gcd(int a, int b ){
+        if ( a < 0 || b < 0 ) return -1;
+        if (b == 0) return a;
+        return gcd(b, a%b);
+    }
+
+
+}
