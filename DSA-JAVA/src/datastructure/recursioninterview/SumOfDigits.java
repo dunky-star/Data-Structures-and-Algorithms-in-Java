7469729fb703c17e4bb323700cb0cd05798b2264
dunky-star <dukygeoffrey39@gmail.com>
2023-02-27 12:44:19 +0300

    Cracking recursion interview quiz - sumOfDigits of a positive number

--- DSA-JAVA/src/datastructure/recursioninterview/SumOfDigits.java
+++ DSA-JAVA/src/datastructure/recursioninterview/SumOfDigits.java
@@ -6,13 +6,12 @@ package datastructure.recursioninterview;
 public class SumOfDigits {
 
     public static void main(String[] args){
-        System.out.println("Sum of Digits: " + sumOfDigit(-33));
+        System.out.println("Sum of Digits: " + sumOfDigit(255));
     }
 
     // Question: Sum of digits of a positive integer number using recursion.
     public static int sumOfDigit(int n){
-        if (n < 0) return -1;
-        if ( n == 0) return 0;
+        if (n < 0 || n == 0 ) return 0;
         return n%10 + sumOfDigit(n/10);
     }
 
