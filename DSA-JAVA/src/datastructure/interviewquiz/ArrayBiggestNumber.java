4f119753432d42f7e932650d1b35fbc5c197ab73
dunky-star <dukygeoffrey39@gmail.com>
2023-02-27 19:40:22 +0300

    Cracking coding interview quiz - Finding biggest number in an array with recursion

--- DSA-JAVA/src/datastructure/interviewquiz/ArrayBiggestNumber.java
+++ DSA-JAVA/src/datastructure/interviewquiz/ArrayBiggestNumber.java
@@ -5,10 +5,10 @@ public class ArrayBiggestNumber {
     public static void main(String[] args){
         int[] sampleArray = {5, 4, 10, 8, 11, 68, 87, 200, -1};
         // Function call.
-        findingBiggestNumber(sampleArray);
+        findBiggestNumber(sampleArray);
     }
 
-    public static void findingBiggestNumber(int[] sampleArray){
+    public static void findBiggestNumber(int[] sampleArray){
         int biggestNumber = sampleArray[0];
         for(int i = 0; i < sampleArray.length; i++){
             if(sampleArray[i] > biggestNumber){
---
+++ DSA-JAVA/src/datastructure/recursioninterview/findMaxNumArray.java
@@ -0,0 +1,17 @@
+package datastructure.recursioninterview;
+
+import static java.lang.Math.max;
+
+public class findMaxNumArray {
+
+    public static void main(String[] args){
+        int[] sampleArray = {5, 4, 10, 8, 11, 68, 87, 200, -1};
+        // Function call.
+      
+    }
+
+    public static int findMaxNumRecursive(int[] sampleArray, int n){
+        if (n == 1) return sampleArray[0];
+        return max(sampleArray[n-1], findMaxNumRecursive(sampleArray, n -1 ) );
+    }
+}
