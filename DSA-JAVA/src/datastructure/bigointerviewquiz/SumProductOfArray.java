97048d352af680669ac4251bcd29f999d444d644
dunky-star <dukygeoffrey39@gmail.com>
2023-02-27 19:54:54 +0300

    Cracking coding interview quiz - Finding sum and product of an array.

---
+++ DSA-JAVA/src/datastructure/bigointerviewquiz/SumProductOfArray.java
@@ -0,0 +1,29 @@
+package datastructure.bigointerviewquiz;
+
+/**
+ * Create a function which calculates the sum and product of
+ * elements of an array.
+ * Find the time complexity for created method.
+ */
+
+public class SumProductOfArray {
+
+    public static void main(String[] args){
+        int[] arrayNum = {5, 4, 10, 8, 11, 68, 87, 200, -1};
+        // Function call.
+        sumAndProductArray(arrayNum);
+    }
+
+    public static void sumAndProductArray(int[] arr){
+        int sum = 0;
+        int product = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        for(int j = 0; j < arr.length; j++){
+            product *= arr[j];
+        }
+        System.out.println("Sum of array is: " + sum + ", the product of array is: " + product);
+    }
+
+}
--- DSA-JAVA/src/datastructure/recursioninterview/findMaxNumArray.java
+++ DSA-JAVA/src/datastructure/recursioninterview/findMaxNumArray.java
@@ -7,7 +7,7 @@ public class findMaxNumArray {
     public static void main(String[] args){
         int[] sampleArray = {5, 4, 10, 8, 11, 68, 87, 200, -1};
         // Function call.
-      
+
     }
 
     public static int findMaxNumRecursive(int[] sampleArray, int n){
