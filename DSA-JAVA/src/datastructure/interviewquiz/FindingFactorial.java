4eaac2de8791f015088d25383a812f796a61e9d2
dunky-star <dukygeoffrey39@gmail.com>
2023-02-27 09:27:15 +0300

    Logic behind recursion

--- DSA-JAVA/src/datastructure/interviewquiz/FindingFactorial.java
+++ DSA-JAVA/src/datastructure/interviewquiz/FindingFactorial.java
@@ -6,11 +6,12 @@ package datastructure.interviewquiz;
 
 public class FindingFactorial {
     public static int factorial(int n){
+        if (n < 0) return -1;
         if(n == 1 || n == 0) return 1;
         return n * factorial(n - 1);
     }
 
     public static void main(String[] args){
-       System.out.println("The factorial is: " + factorial(5)) ;
+       System.out.println("The factorial is: " + factorial(10)) ;
     }
 }
