ec38dbe0ec877e5683109dada77ccd1316c84eac
dunky-star <dukygeoffrey39@gmail.com>
2023-03-01 16:59:21 +0300

    Array data structure

---
+++ DSA-JAVA/src/datastructure/arrays/ArraysDataStructure.java
@@ -0,0 +1,11 @@
+package datastructure.arrays;
+
+/**
+ * What is an array?
+ * An array is simply a collection of object or things of same type.
+ * Each object can be uniquely identified based on their location.
+ * The size cannot be changed.
+ */
+
+public class ArraysDataStructure {
+}
