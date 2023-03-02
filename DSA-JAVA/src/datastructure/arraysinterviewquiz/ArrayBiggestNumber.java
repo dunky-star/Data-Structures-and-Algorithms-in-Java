decabe04b87dc75d2f4bd0df24eb40db352c951f
dunky-star <dukygeoffrey39@gmail.com>
2023-03-02 13:08:34 +0300

    Cracking array interview quiz

--- DSA-JAVA/src/datastructure/arraysinterviewquiz/ArrayBiggestNumber.java
+++ DSA-JAVA/src/datastructure/arraysinterviewquiz/ArrayBiggestNumber.java
@@ -1,20 +1,20 @@
-package datastructure.interviewquiz;
+package datastructure.arraysinterviewquiz;
 
 public class ArrayBiggestNumber {
 
     public static void main(String[] args){
         int[] sampleArray = {5, 4, 10, 8, 11, 68, 87, 200, -1};
         // Function call.
-        findBiggestNumber(sampleArray);
+        System.out.println("The biggest number is: " + findBiggestNumber(sampleArray));
     }
 
-    public static void findBiggestNumber(int[] sampleArray){
+    public static int findBiggestNumber(int[] sampleArray){
         int biggestNumber = sampleArray[0];
         for(int i = 0; i < sampleArray.length; i++){
             if(sampleArray[i] > biggestNumber){
                 biggestNumber = sampleArray[i];
             }
         }
-        System.out.println("The biggest number is: " + biggestNumber);
+        return  biggestNumber;
     }
 }
