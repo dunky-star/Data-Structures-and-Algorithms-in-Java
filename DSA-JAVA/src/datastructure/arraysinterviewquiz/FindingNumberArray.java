c34423301e22dacf9368fbc5c16f2832c1c4a03d
dunky-star <dukygeoffrey39@gmail.com>
2023-03-02 15:28:19 +0300

    Cracking array interview quiz

--- DSA-JAVA/src/datastructure/arraysinterviewquiz/FindingNumberArray.java
+++ DSA-JAVA/src/datastructure/arraysinterviewquiz/FindingNumberArray.java
@@ -10,19 +10,19 @@ public class FindingNumberArray {
     public static void main(String[] args){
 
         int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println("Number found: " + linearSearch(intArray, 9));
-
+        System.out.println("Number found at index: " + linearSearch(intArray, 8));
 
     }
 
     // Array linear search.
-    public static boolean linearSearch(int[] intArray, int value){
-        for (int i : intArray) {
-            if (i == value) {
-                return true;
+    public static int linearSearch(int[] intArray, int value){
+
+        for(int i = 0; i < intArray.length; i++) {
+            if (intArray[i] == value) {
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 
 }
