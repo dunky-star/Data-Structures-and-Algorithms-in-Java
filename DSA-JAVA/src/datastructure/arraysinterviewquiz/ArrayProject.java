02df47a8e6fead0b9dbb8515a792e97a2e8c7bcc
dunky-star <dukygeoffrey39@gmail.com>
2023-03-02 09:26:00 +0300

    Average temperature calc.

--- DSA-JAVA/src/datastructure/arraysinterviewquiz/ArrayProject.java
+++ DSA-JAVA/src/datastructure/arraysinterviewquiz/ArrayProject.java
@@ -3,21 +3,33 @@ package datastructure.arraysinterviewquiz;
 import java.util.Scanner;
 
 /**
- * Calculate average temperature/ Find the number of days above average temperature.
+ * Calculate average temperature and find the number of days above average temperature.
  */
+
 public class ArrayProject {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("How many days' temperature? ");
         int numOfDays = sc.nextInt();
+        int[] temps = new int[numOfDays];
+        // Record temperature and find average
         int sum = 0;
-        for (int i = 0; i <= numOfDays; i++){
-            System.out.println("Day " + i + " 's high temp: ");
-            int next = sc.nextInt();
-            sum += next;
+        for (int i = 0; i < numOfDays; i++){
+            System.out.println("Day " + (i+1) + " 's high temp: ");
+            temps[i] = sc.nextInt();
+            sum += temps[i];
         }
         double average = (double) sum / numOfDays;
         System.out.println();
+        // Count days above average.
+        int above = 0;
+        for (int i = 0; i < temps.length; i++){
+            if (temps[i]>average){
+                above++;
+            }
+        }
         System.out.println("Average temp: " + average);
+        System.out.println(above + " Days above average");
+
     }
 }
