ef05189098b3983419c9e24c0dcbf7affd7b9150
dunky-star <dukygeoffrey39@gmail.com>
2023-03-02 16:48:03 +0300

    Cracking array interview quiz

--- DSA-JAVA/src/datastructure/arraysinterviewquiz/IsUniqueDuplicate.java
+++ DSA-JAVA/src/datastructure/arraysinterviewquiz/IsUniqueDuplicate.java
@@ -1,9 +1,30 @@
 package datastructure.arraysinterviewquiz;
 
+/**
+ * Write a program to check if an array is unique or not.
+ *
+ *         Example
+ *
+ *         int[] intArray = {1,2,3,4,5,6};
+ *         isUnique(intArray) // true
+ */
 public class IsUniqueDuplicate {
     public static void main(String[] args){
+        int[] intArray = {1, 2, 3, 4, 5, 10, 20, 0};
+        System.out.println("Array is Unique: " + isUnique(intArray));
 
     }
 
+    // Is unique
+    public static boolean isUnique(int[] intArray){
+        for(int i = 0; i < intArray.length; i++){
+            for(int j= i+1; j < intArray.length; j++){
+                if(intArray[i] ==  intArray[j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 }
