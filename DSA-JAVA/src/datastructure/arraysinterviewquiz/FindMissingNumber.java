92b5b8979303032ba04bb63c4be335007c99568e
dunky-star <dukygeoffrey39@gmail.com>
2023-03-02 09:35:59 +0300

    Cracking array interview quiz

---
+++ DSA-JAVA/src/datastructure/arraysinterviewquiz/FindMissingNumber.java
@@ -0,0 +1,13 @@
+package datastructure.arraysinterviewquiz;
+
+/**
+ * Find the missing number in an integer array of 1 to 10.
+ *
+ * Example for 10 elements
+ *
+ * int intArray[] = {1, 2, 3, 4, 5, 6, 8, 9, 10};
+ */
+
+public class FindMissingNumber {
+    
+}
