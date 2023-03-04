42241401493eb788ca798b802feaaee54cccd83c
dunky-star <dukygeoffrey39@gmail.com>
2023-03-04 10:32:39 +0300

    Cracking interview quiz - Linked List

---
+++ DSA-JAVA/src/datastructure/linkedlistinterviewquiz/Main.java
@@ -0,0 +1,7 @@
+package datastructure.linkedlistinterviewquiz;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ DSA-JAVA/src/datastructure/linkedlistinterviewquiz/Node.java
@@ -0,0 +1,7 @@
+package datastructure.linkedlistinterviewquiz;
+
+public class Node {
+    public int value;
+    public Node next;
+    public Node prev;
+}
