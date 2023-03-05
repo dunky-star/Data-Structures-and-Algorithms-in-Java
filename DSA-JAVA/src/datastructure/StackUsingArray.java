98b43983c8889e84c6624ad7e2be507c6d0f93d7
dunky-star <dukygeoffrey39@gmail.com>
2023-03-05 12:37:39 +0300

    Stack data structure

--- DSA-JAVA/src/datastructure/StackUsingArray.java
+++ DSA-JAVA/src/datastructure/StackUsingArray.java
@@ -1,15 +1,38 @@
 package datastructure;
 
 public class StackUsingArray {
-    public static void main(String[] args){
-        StackUsingArray newStackArray = new StackUsingArray(5);
-    }
-    int[] arr;
+     int[] arr;
     int topOfStck;
 
-    public StackUsingArray(int size){
+    protected StackUsingArray(int size){
         this.arr = new int[size];
         this.topOfStck = -1;
         System.out.println("The stack is created with size of: " + size);
+    }
+
+    // isEmpty
+    protected boolean isEmpty(){
+        if(topOfStck == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // isFull
+    protected boolean isFull(){
+        if(topOfStck == arr.length -1){
+            System.out.println("The stack is full");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Main method - program entry point.
+    public static void main(String[] args){
+        StackUsingArray newStackArray = new StackUsingArray(5);
+        boolean result = newStackArray.isFull();
+        System.out.println("Stack is Full: " + result);
     }
 }
