f0c480f4a9d0a2b6dabce0df574e643ee916e5d5
dunky-star <dukygeoffrey39@gmail.com>
2023-03-05 12:56:17 +0300

    Stack data structure

--- DSA-JAVA/src/datastructure/StackUsingArray.java
+++ DSA-JAVA/src/datastructure/StackUsingArray.java
@@ -2,17 +2,17 @@ package datastructure;
 
 public class StackUsingArray {
      int[] arr;
-    int topOfStck;
+    int topOfStack;
 
     protected StackUsingArray(int size){
         this.arr = new int[size];
-        this.topOfStck = -1;
+        this.topOfStack = -1;
         System.out.println("The stack is created with size of: " + size);
     }
 
     // isEmpty
     protected boolean isEmpty(){
-        if(topOfStck == -1){
+        if(topOfStack == -1){
             return true;
         }else{
             return false;
@@ -21,7 +21,7 @@ public class StackUsingArray {
 
     // isFull
     protected boolean isFull(){
-        if(topOfStck == arr.length -1){
+        if(topOfStack == arr.length -1){
             System.out.println("The stack is full");
             return true;
         }else{
@@ -29,10 +29,36 @@ public class StackUsingArray {
         }
     }
 
+    // Push
+    protected void push(int value){
+        if(isFull()){
+            System.out.println("The stack is full");
+        }else{
+            arr[topOfStack +1] = value;
+            topOfStack++;
+        }
+    }
+
+    // Pop
+    protected int pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }else{
+            int topStack = arr[topOfStack];
+            topOfStack--;
+            return topStack;
+        }
+    }
+
     // Main method - program entry point.
     public static void main(String[] args){
         StackUsingArray newStackArray = new StackUsingArray(5);
-        boolean result = newStackArray.isFull();
+        newStackArray.push(10);
+        newStackArray.push(25);
+        newStackArray.push(5);
+        newStackArray.push(40);
+        int result = newStackArray.pop();
         System.out.println("Stack is Full: " + result);
     }
 }
