409a1a278f1e288df2e7a9e22a4b8bebecbbb8f5
dunky-star <dukygeoffrey39@gmail.com>
2023-03-07 09:57:24 +0300

    Searching algorithms

--- DSA-JAVA/src/datastructure/algorithms/searching/LinearSearch.java
+++ DSA-JAVA/src/datastructure/algorithms/searching/LinearSearch.java
@@ -1,4 +1,11 @@
 package datastructure.algorithms.searching;
 
+/**
+ * When to use?
+ * - When we have unsorted arrays/data, Linear search is good.
+ * When not to use?
+ * - When we have sorted array or data.
+ */
+
 public class LinearSearch {
 }
