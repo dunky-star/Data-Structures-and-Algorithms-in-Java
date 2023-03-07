62952c1ed930a581e6ab0291dc7620aa5edccbbd
dunky-star <dukygeoffrey39@gmail.com>
2023-03-07 15:32:57 +0300

    Divide and Conquer algorithms

--- DSA-JAVA/src/datastructure/algorithms/dividenconquer/LongestPalindromicSubsequence.java
+++ DSA-JAVA/src/datastructure/algorithms/dividenconquer/LongestPalindromicSubsequence.java
@@ -10,5 +10,31 @@ package datastructure.algorithms.dividenconquer;
  */
 
 public class LongestPalindromicSubsequence {
-    
+    private int findLPSLength(String st, int startIndex, int endIndex) {
+        if (startIndex > endIndex) {
+            return 0;
+        }
+        if (startIndex == endIndex) {
+            return 1;
+        }
+
+        int count1 = 0;
+        if (st.charAt(startIndex) == st.charAt(endIndex)) {
+            count1 = 2 + findLPSLength(st, startIndex+1, endIndex-1);
+        }
+        int count2 = findLPSLength(st, startIndex+1, endIndex);
+        int count3 = findLPSLength(st, startIndex, endIndex-1);
+
+        return Math.max(count1, Math.max(count2, count3));
+    }
+
+    public int findLPSLength(String st) {
+        return findLPSLength(st, 0, st.length()-1);
+    }
+
+    // Main method
+    public static void main(String[] args){
+        LongestPalindromicSubsequence lps = new LongestPalindromicSubsequence();
+        System.out.println("Longest Palindromic Sequence: " +lps.findLPSLength("elrmenmet"));
+    }
 }
