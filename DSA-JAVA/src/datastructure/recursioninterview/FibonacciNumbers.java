8e81f97406f8ae7c2e7e97f3b446d0d66d87c118
dunky-star <dukygeoffrey39@gmail.com>
2023-03-07 18:20:54 +0300

    Dynamic programming algorithms - Fibonacci numbers using Memoization and Tabulation

--- DSA-JAVA/src/datastructure/recursioninterview/FibonacciNumbers.java
+++ DSA-JAVA/src/datastructure/recursioninterview/FibonacciNumbers.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 /**
  * Write a program to return a number in the series 0,1,1,3,5,8,13,...,n.
- * Write a recursive function called fib which accepts a number and returns
+ * Write a recursive function called fibMemo which accepts a number and returns
  * the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence
  * of whole numbers 0,1, 1, 2, 3, 5, 8, ... which starts with 0 and 1,
  * and where every number thereafter is equal to the sum of the previous two numbers.
