c907338617804a11094ae68ebee91c18ae886760
dunky-star <dukygeoffrey39@gmail.com>
2023-03-07 17:58:42 +0300

    Dynamic programming algorithms - Fibonacci numbers using Memoization

--- DSA-JAVA/src/datastructure/recursioninterview/FibonacciNumbers.java
+++ DSA-JAVA/src/datastructure/recursioninterview/FibonacciNumbers.java
@@ -1,5 +1,7 @@
 package datastructure.recursioninterview;
 
+import java.util.HashMap;
+
 /**
  * Write a program to return a number in the series 0,1,1,3,5,8,13,...,n.
  * Write a recursive function called fib which accepts a number and returns
@@ -8,18 +10,24 @@ package datastructure.recursioninterview;
  * and where every number thereafter is equal to the sum of the previous two numbers.
  */
 
-// This problem is Fibonacci series.
+// This problem is Fibonacci series using memoization.
 public class FibonacciNumbers {
 
-    public int fib(int n){
-        if (n < 0) return -1;
-        if (n == 0 || n == 1) return n;
-        return fib(n - 1) + fib(n - 2);
+    public static int fibMemo(int n, HashMap<Integer, Integer> memo){
+        if(n == 1) return 0;
+
+        if(n == 2) return 1;
+
+        if(!memo.containsKey(n)){
+            memo.put(n, (fibMemo(n-1, memo) + fibMemo(n-2, memo)));
+        }
+        return memo.get(n);
+
     }
 
     public static void main(String[] args){
-        FibonacciNumbers fibonacci = new FibonacciNumbers();
-        int result = fibonacci.fib(6);
-        System.out.println("Result: " + result);
+
+        HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
+        System.out.println("Result: " + fibMemo(6, memo));
     }
 }
