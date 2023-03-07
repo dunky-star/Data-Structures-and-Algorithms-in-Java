06da4b6225a9ffa4b8a94eb9d402dda55faa7e42
dunky-star <dukygeoffrey39@gmail.com>
2023-03-07 15:46:39 +0300

    Divide and Conquer algorithms

--- DSA-JAVA/src/datastructure/algorithms/dividenconquer/LongestPalindromicSubsequence.java
+++ DSA-JAVA/src/datastructure/algorithms/dividenconquer/LongestPalindromicSubsequence.java
@@ -4,8 +4,8 @@ package datastructure.algorithms.dividenconquer;
  * Problem statement:
  * S is a given string.
  * Find the longest palindromic subsequence (LPS).
- * -  Subsequence - is a sequence that can be derived from another sequence by deleting some elements
- * without changing the order of them.
+ * -  Subsequence - is a sequence that can be derived from another sequence
+ * by deleting some elements without changing the order of them.
  * - Palindrome is a string that reads the same backward as well as forward.
  */
 
