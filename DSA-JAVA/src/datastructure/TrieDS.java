a6af0b7518e2db5f5d31ff83254eac258b0eee6c
dunky-star <dukygeoffrey39@gmail.com>
2023-03-09 20:44:45 +0300

    Trie Data Structure

--- DSA-JAVA/src/datastructure/TrieDS.java
+++ DSA-JAVA/src/datastructure/TrieDS.java
@@ -39,12 +39,76 @@ public class TrieDS {
         System.out.println("Successfully inserted " + word+ " in Trie");
     }
 
+    // Search for a word in Trie
+
+    public boolean search(String word) {
+        Node currentNode = root;
+        for (int i =0; i<word.length(); i++) {
+            char ch = word.charAt(i);
+            Node node = currentNode.children.get(ch);
+            if (node == null) {
+                System.out.println("Word: "+word+ " does not exist in Trie");
+                return false;
+            }
+            currentNode = node;
+        }
+        if (currentNode.endOfString == true) {
+            System.out.println("Word: "+word+ " exists in Trie");
+            return true;
+        } else {
+            System.out.println("Word: "+word+ " does not exist in Trie. But it is a prefix of another string");
+        }
+        return currentNode.endOfString;
+    }
+
+    // Delete a String from Trie
+
+    private boolean delete(Node parentNode, String word, int index) {
+        char ch = word.charAt(index);
+        Node currentNode = parentNode.children.get(ch);
+        boolean canThisNodeBeDeleted;
+
+        if (currentNode.children.size() > 1) {
+            delete(currentNode, word, index+1);
+            return false;
+        }
+        if (index == word.length() -1) {
+            if (currentNode.children.size()>=1) {
+                currentNode.endOfString = false;
+                return false;
+            } else {
+                parentNode.children.remove(ch);
+                return true;
+            }
+        }
+        if (currentNode.endOfString == true) {
+            delete(currentNode, word, index+1);
+            return false;
+        }
+        canThisNodeBeDeleted = delete(currentNode, word, index+1);
+        if (canThisNodeBeDeleted == true) {
+            parentNode.children.remove(ch);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void delete(String word) {
+        if (search(word) == true) {
+            delete(root, word, 0);
+        }
+    }
+
     // Main class
     public static void main(String[] args){
 
         TrieDS newTrie = new TrieDS();
-
-
+        newTrie.insert("API");
+        newTrie.insert("APIS");
+        newTrie.insert("APPLE");
+        newTrie.search("API");
+        newTrie.search("APS");
 
     }
 }
