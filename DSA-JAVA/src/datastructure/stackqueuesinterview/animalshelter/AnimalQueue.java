ffb74259a68717d728fb0dfb25612f809ff3fa8c
dunky-star <dukygeoffrey39@gmail.com>
2023-03-10 14:55:46 +0300

    Cracking coding interview - Stacks and Queues

--- DSA-JAVA/src/datastructure/stackqueuesinterview/animalshelter/AnimalQueue.java
+++ DSA-JAVA/src/datastructure/stackqueuesinterview/animalshelter/AnimalQueue.java
@@ -23,14 +23,40 @@ public class AnimalQueue {
     }
 
     // Dequeue dog
-    public Dog dequeueDog(){
+    public Dog dequeueDogs(){
         return dogs.poll();
     }
 
     // Peek dog
-    public Dog peekDog(){
+    public Dog peekDogs(){
         return dogs.peek();
     }
 
+    // Dequeue cat
+    public Cat dequeueCats(){
+        return cats.poll();
+    }
 
+    // Peek cat
+    public Cat peekCats(){
+        return cats.peek();
+    }
+
+    // Dequeue any
+    public Animal dequeueAny(){
+        if(dogs.size()==0){
+            return dequeueCats();
+        }else if(cats.size()==0){
+            return dequeueDogs();
+        }
+        Dog dog = dogs.peek();
+        Cat cat = cats.peek();
+        if(dog.isOlderThan(cat)){
+            return dogs.poll();
+        }else {
+            return cats.poll();
+        }
+    }
+
+    
 }
