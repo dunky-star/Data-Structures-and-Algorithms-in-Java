6828c4b549a1010ca3ec14f781266efc268db88b
dunky-star <dukygeoffrey39@gmail.com>
2023-03-10 11:05:07 +0300

    Cracking interview quiz - Queues and Stacks

---
+++ DSA-JAVA/src/datastructure/stackqueuesinterview/animalshelter/Animal.java
@@ -0,0 +1,20 @@
+package datastructure.stackqueuesinterview.animalshelter;
+
+public abstract class Animal {
+    private int order;
+    protected String name;
+
+    public Animal(String n){
+        name = n;
+    }
+
+    public abstract String name();
+
+    public void setOrder(int ord){
+        order = ord;
+    }
+
+    public int getOrder(){
+        return order;
+    }
+}
